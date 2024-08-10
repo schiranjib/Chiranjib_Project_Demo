0b6d5a74e26fd03a2b55cf0bfd7dbe0385a5e21f
symbiangit <chiranjibshoping@gmail.com>
2024-08-10 16:32:53 +0530

    add initial file readme author updated

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Chiranjib_Project_Demo
 # this is project information document
 we are creating a shoping website
+interface design enhanced by mr. arun
---
+++ sub-function.java
@@ -0,0 +1,16 @@
+// Java program to demonstrate working
+// of java.lang.Math.subtractExact() method
+
+import java.lang.Math;
+
+class Gfg1 {
+
+	// driver code
+	public static void main(String args[])
+	{
+		int a = 300;
+		int b = 200;
+
+		System.out.println(Math.subtractExact(a, b));
+	}
+}
